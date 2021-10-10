faf2b98396a1f6e8206dbf4c7dd1a0a062dbad0f
unknown <nero0213@naver.com>
2021-10-10 19:02:43 +0900

    Used requests Module

--- navapi.py
+++ navapi.py
@@ -1,34 +1,31 @@
-import urllib.request
+import requests
 import pymongo
 
+#----------------웹에서 입력을 받아야 하는 uri 파라미터정보--------------------
+search_title = input('제목 : ')
+search_yearfrom = input('몇년도부터? :')
+search_yearto = input('몇년도까지? : ')
+#--------------추후에 input 대신에 웹에서 불러온 정보를 대입하여 사용------------- 
+
+#MongoDB 정보
 conn = pymongo.MongoClient("localhost",27017)
 db = conn.movie
 col = db.foundmovie
-
-client_id = '_emDW747XTNkGrS69ARU'
-client_secret = 'a1hsFw_cOO'
-
+#uri정보
+headers = {'X-Naver-Client-Id' : '_emDW747XTNkGrS69ARU','X-Naver-Client-Secret':'a1hsFw_cOO'}
 url = 'https://openapi.naver.com/v1/search/movie.json'
-query = "?query="+urllib.parse.quote(input("질의:"))
-query1 = "&yearfrom="+urllib.parse.quote(input("몇년도부터?:"))
-query2 = "&yearto="+urllib.parse.quote(input("몇년도까지?:"))
-option = '&display=5'
+params = {'query' : search_title , 'yearfrom':search_yearfrom , 'yearto':search_yearto , 'display':'5'}
 
-url_query = url + query + query1 + query2 + option
+response = requests.get(url,headers=headers,params=params)
+rescode = response.status_code
+result = list(response.json().values())[4]
 
-request = urllib.request.Request(url_query)
-request.add_header("X-Naver-Client-Id",client_id)
-request.add_header("X-Naver-Client-Secret",client_secret)
 
-response = urllib.request.urlopen(request)
-rescode = response.getcode()
+
 if(rescode == 200):
-    response_body = response.read()
-    print(response_body.decode('utf-8'))
+    print('Saved at DB!')
+    col.insert_many(result)
+    #print(result)                                       #원할 시, 영화 관련 정보만 출력
+    #print(response.content.decode('utf-8'))           #원할 시, 정제하지 않은 전체 응답 내용을 출력
 else:
     print("Error code:"+rescode)
-
-
-result = response_body.decode('utf-8')
-
-print('result : ',type(response))
